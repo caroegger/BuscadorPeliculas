a7e13b25b0ebde18f3138e233e5b1ea89dc215f8
caroegger <carolaeggr@gmail.com>
2021-06-11 13:47:29 -0300

    buscador solo funciona con la primer busqueda

--- src/components/Buscar.js
+++ src/components/Buscar.js
@@ -1,6 +1,44 @@
+import {useState, useEffect} from "react";
+import ListaPeliculas from "./ListaPeliculas";
+
 const Buscar = () => {
+
+    const [busqueda, setBusqueda] = useState("")
+    const [input, setInput] = useState("")
+    const [resultado, setResultado] = useState([])
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=2d9a1d2ba98bd4f269229908068559f1&languaje=es-ES&query=${input}`)
+        .then (res => res.json())
+        .then (data => {
+        setResultado(data.results)})
+    }, [input] )    
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setBusqueda(input)
+    }
+    
+    const handleChange = (e) => {
+        setInput(e.target.value)
+    }
+
+    console.log(busqueda)
+    console.log(input)
+    console.log(resultado)
+
     return (
-        <div>ESTOY EN BUSCAR</div>
+        <>
+        <p>Busca tu pelicula</p>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input value={input} onChange={handleChange}></input>
+            </form>
+        </div>
+        <div>
+            [<ListaPeliculas peliculas={resultado}></ListaPeliculas>]
+        </div>
+        </>
     )
 }
 
